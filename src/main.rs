76987b4185af4ec8852aa517cf05dfb1e9da2494
Alexander Wood <alexljwood24@hotmail.co.uk>
2024-09-02 17:27:26 -0400

    Setup basic example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use sandkasten_client::{
+    schemas::programs::{BuildRequest, BuildRunRequest, MainFile},
+    SandkastenClient,
+};
+
+#[tokio::main]
+async fn main() {
+    let client = SandkastenClient::new("https://sandkasten.bootstrap.academy/".parse().unwrap());
+    let result = client
+        .build_and_run(&BuildRunRequest {
+            build: BuildRequest {
+                environment: "python".into(),
+                main_file: MainFile {
+                    name: Some("test.py".into()),
+                    content: "print(6 * 7, end='')".into(),
+                },
+                ..Default::default()
+            },
+            run: Default::default(),
+        })
+        .await
+        .unwrap();
+    assert_eq!(result.run.stdout, "42");
 }
